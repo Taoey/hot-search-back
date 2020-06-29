3d5284e139add76212ae19b265c08c76026089be
taoey <741494582@qq.com>
2020-06-29 10:39:44 +0800

    PREF 优化配置加载

--- pkg/sysinit/init_config.go
+++ pkg/sysinit/init_config.go
@@ -1,14 +1,18 @@
 package sysinit
 
 import (
+	"fmt"
 	"github.com/olebedev/config"
-	"os"
 )
 
 var GCF *config.Config //global config
 
 func InitConf() {
-	pwd, _ := os.Getwd()
-	configPath := pwd + "/configs/application.yml"
-	GCF, _ = config.ParseYamlFile(configPath)
+	configPath := "./configs/application.yml"
+	//configPath := `G:\坚果云同步\github\1_projects\hot-search-back\configs\application.yml`
+	var err error
+	GCF, err = config.ParseYamlFile(configPath)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
