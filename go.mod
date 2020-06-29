4abdd262b309931fa8cd275bda2f8d1575e24a71
taoey <741494582@qq.com>
2020-06-29 10:55:40 +0800

    FEATURE 使用日期进行日志分割

--- go.mod
+++ go.mod
@@ -4,16 +4,21 @@ go 1.14
 
 require (
 	github.com/bamzi/jobrunner v0.0.0-20190912144227-6e09e010a32d
+	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/iris-contrib/middleware/cors v0.0.0-20191219204441-78279b78a367
+	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
 	github.com/jinzhu/gorm v1.9.14
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/kataras/iris/v12 v12.1.9-0.20200621141528-f6355c9716ab
+	github.com/lestrrat-go/file-rotatelogs v2.3.0+incompatible
+	github.com/lestrrat-go/strftime v1.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/olebedev/config v0.0.0-20190528211619-364964f3a8e4
 	github.com/onsi/ginkgo v1.10.1 // indirect
 	github.com/onsi/gomega v1.7.0 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337 // indirect
+	github.com/tebeka/strftime v0.1.4 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/yudai/pp v2.0.1+incompatible // indirect
 	go.uber.org/zap v1.15.0
--- pkg/sysinit/init_logger.go
+++ pkg/sysinit/init_logger.go
@@ -2,9 +2,10 @@ package sysinit
 
 import (
 	"fmt"
+	"github.com/lestrrat-go/file-rotatelogs"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"gopkg.in/natefinch/lumberjack.v2"
+	"io"
 	"os"
 	"strings"
 	"time"
@@ -13,14 +14,14 @@ import (
 var LOG *zap.SugaredLogger
 
 func InitLogger() {
-	// 日志文件
-	hook := lumberjack.Logger{
-		Filename:   GCF.UString("logger.path", "./logs/log"), // 日志文件路径
-		MaxSize:    5,                                        // 每个日志文件保存的最大尺寸 单位：M
-		MaxBackups: 30,                                       // 日志文件最多保存多少个备份
-		MaxAge:     7,                                        // 文件最多保存多少天
-		Compress:   true,                                     // 是否压缩
-	}
+	// 日志文件保存及分割：按照日志大小进行分割
+	//hook := lumberjack.Logger{
+	//	Filename:   GCF.UString("logger.path", "./logs/log"), // 日志文件路径
+	//	MaxSize:    5,                                        // 每个日志文件保存的最大尺寸 单位：M
+	//	MaxBackups: 30,                                       // 日志文件最多保存多少个备份
+	//	MaxAge:     7,                                        // 文件最多保存多少天
+	//	Compress:   true,                                     // 是否压缩
+	//}
 
 	// 日志格式
 	encoderConfig := zapcore.EncoderConfig{
@@ -65,9 +66,13 @@ func InitLogger() {
 	} else {
 		styleEncoder = zapcore.NewConsoleEncoder(encoderConfig)
 	}
+
+	filelogWriter := getWriter(GCF.UString("logger.path", "./logs/log"))
+
 	core := zapcore.NewCore(
 		styleEncoder,
-		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook)), // 打印到控制台和文件
+		//zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook)), // 打印到控制台和文件
+		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(filelogWriter)), // 打印到控制台和文件
 		atomicLevel, // 日志级别
 	)
 
@@ -88,4 +93,23 @@ func InitLogger() {
 // 自定义日期样式
 func TimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(fmt.Sprintf("[%v]", t.Format("2006-01-02 15:04:05")))
+}
+
+// 日志分割器：按照日期进行分割
+// 参考自：https://www.cnblogs.com/ExMan/p/12264925.html
+func getWriter(filename string) io.Writer {
+	// 生成rotatelogs的Logger 实际生成的文件名 demo.log.YYmmddHH
+	// demo.log是指向最新日志的链接
+	// 保存7天内的日志，每1小时(整点)分割一次日志
+	hook, err := rotatelogs.New(
+		filename+".%Y%m%d%H", // 没有使用go风格反人类的format格式
+		rotatelogs.WithLinkName(filename),
+		rotatelogs.WithMaxAge(time.Hour*24*7),
+		rotatelogs.WithRotationTime(time.Hour),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	return hook
 }
