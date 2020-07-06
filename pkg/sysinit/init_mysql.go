aa200c9c91f3ce5e9938e29fc44da35f00118a22
taoey <hwt8080@163.com>
2020-07-06 20:16:43 +0800

    PREF 优化mysql，开启日志模式，对接日志到zap

--- pkg/sysinit/init_mysql.go
+++ pkg/sysinit/init_mysql.go
@@ -15,5 +15,22 @@ func InitMysql() {
 	db.DB().SetMaxIdleConns(GCF.UInt("mysql.max_idle", 10))  //空闲最大连接数
 	db.DB().SetMaxOpenConns(GCF.UInt("mysql.max_open", 100)) //最大连接数
 
-	MysqlSession = db.DB()
+	db.LogMode(true)
+	db.SetLogger(Logger{})
+	MysqlSession = db
+
+	//fmt.Println("mysql init end",db)
+}
+
+type Logger struct {
+}
+
+//level := values[0]    // 操作级别
+//filePath := values[1] // 文件路径
+//time := values[2]     // 执行时间
+//sql := values[3]      // sql语句
+//params := values[4]   // 查询的参数
+//rows := values[5]     // 查询出来的数据条数
+func (logger Logger) Print(values ...interface{}) {
+	LOG.Debugf("%v %v %v %v", values[2], values[3], values[4], values[1]) // 这里输出的就是日志内容
 }
